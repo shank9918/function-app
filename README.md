7f07a2d8b1bfa5fde67a66b77a025d5fc7ce438f
Shashank <shashankgowri@gmail.com>
2020-12-21 14:50:58 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# function-app
+- Azure Function built using Java
+- Uses azure-functions-maven-plugin to get the function published to Azure
+
+# References
+- [Develop an App using the Maven Plugin for Azure Functions](https://docs.microsoft.com/en-us/learn/modules/develop-azure-functions-app-with-maven-plugin/)
+- [Create your first Azure function with Java and IntelliJ](https://docs.microsoft.com/en-us/azure/azure-functions/functions-create-maven-intellij)
---
+++ src/main/java/com/shashankg/azure/cert/functions/HttpTriggerFunction.java
@@ -0,0 +1,48 @@
+package com.shashankg.azure.cert.functions;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+
+import java.util.Optional;
+
+/**
+ * Azure Functions with HTTP Trigger.
+ */
+public class HttpTriggerFunction {
+	/**
+	 * This function listens at endpoint "/api/HttpExample". Two ways to invoke it using "curl" command in bash:
+	 * 1. curl -d "HTTP Body" {your host}/api/HttpExample
+	 * 2. curl "{your host}/api/HttpExample?name=HTTP%20Query"
+	 */
+	@FunctionName("HttpExample")
+	public HttpResponseMessage run(
+			@HttpTrigger(
+					name = "req",
+					methods = {HttpMethod.GET, HttpMethod.POST},
+					authLevel = AuthorizationLevel.ANONYMOUS)
+					HttpRequestMessage<Optional<String>> request,
+			final ExecutionContext context) {
+		context.getLogger().info("Java HTTP trigger processed a request.");
+
+		// Parse query parameter
+		final String nameQuery = request.getQueryParameters().get("name");
+		final String lastNameQuery = request.getQueryParameters().get("lastname");
+		final String name = request.getBody().orElse(nameQuery);
+		final String lastName = request.getBody().orElse(lastNameQuery);
+		context.getLogger().info("URI: " + request.getUri());
+		context.getLogger().info("Method: " + request.getHttpMethod());
+		if (name == null) {
+			return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
+		} else if (lastName == null) {
+			return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a lastname on the query string or in the request body").build();
+		} else {
+			return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name + ' ' + lastName).build();
+		}
+	}
+}
